28d1f85488a2ac79ecc2f40898218e17a4b9cde7
YomYomNuts <noisette.guillaume@gmail.com>
2017-01-04 16:48:10 +0100

    Create MicrophoneInput.cs

---
+++ MicrophoneInput.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour
+{
+    public static float MicLoudness;
+
+    private string _device;
+    bool _isInitialized;
+    AudioClip _clipRecord = new AudioClip();
+    int _sampleWindow = 128;
+
+    void InitMic()
+    {
+        if (_device == null) _device = Microphone.devices[0];
+        _clipRecord = Microphone.Start(_device, true, 999, 44100);
+    }
+
+    void StopMicrophone()
+    {
+        Microphone.End(_device);
+    }
+
+    float LevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(null) - (_sampleWindow + 1);
+        if (micPosition < 0) return 0;
+        _clipRecord.GetData(waveData, micPosition);
+        for (int i = 0; i < _sampleWindow; ++i)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+    void Update()
+    {
+        MicLoudness = LevelMax();
+    }
+
+    void OnEnable()
+    {
+        InitMic();
+        _isInitialized = true;
+    }
+
+    void OnDisable()
+    {
+        StopMicrophone();
+    }
+
+    void OnDestroy()
+    {
+        StopMicrophone();
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            if (!_isInitialized)
+            {
+                InitMic();
+                _isInitialized = true;
+            }
+        }
+        if (!focus)
+        {
+            StopMicrophone();
+            _isInitialized = false;
+        }
+    }
+}
